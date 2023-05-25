b9326d1ad567889eb80562fac50f7e9a7b24ca9d
Ildar1981 <fahriyevi@yandex.ru>
2023-05-25 21:31:53 +0300

    changes

--- specs/test_a.spec.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch'
-test('GET', async () => {
-    const response = fetch('https://dummyjson.com/products')
-    const data = await response.json();
-    console.log(data)
-   
-});
